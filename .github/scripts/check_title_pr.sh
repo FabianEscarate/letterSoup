b60577fd2c5ff929ac9fac44868d155b25838d40
Fabian Escarate <fescarate@outlook.com>
2024-09-10 15:26:12 -0300

    add jobs to workflows

--- .github/scripts/check_title_pr.sh
+++ .github/scripts/check_title_pr.sh
@@ -7,18 +7,21 @@ echo "Title of pr $pr_title"
 
 if [[ "$pr_title" =~ ^\[major\].?+$ ]]; then
   echo "has major in title, generate a major version"
+  echo "npm_version_flag=major" >> "$GITHUB_OUTPUT"
   exit 0
 fi
 
 if [[ "$pr_title" =~ ^\[minor\].?+$ ]]; then
   echo "has minor in title, generate a minor version"
+  echo "npm_version_flag=minor" >> "$GITHUB_OUTPUT"
   exit 0
 fi
 
 if [[ "$pr_title" =~ ^\[patch\].?+$ ]]; then
   echo "has patch in title, generate a patch version"
+  echo "npm_version_flag=patch" >> "$GITHUB_OUTPUT"
   exit 0
 fi
 
-echo "::error title={Error Script}::Error, PR Title must be start with [patch], [minor] or [major]"
+echo "::error title=Error Script::Error, PR Title must be start with [patch], [minor] or [major]"
 exit 1
